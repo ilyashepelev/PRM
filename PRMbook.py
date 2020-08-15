a377102eff74cd5e9f41672e70ffae2cce8f8748
Stril2 <ilyashepelev@mail.ru>
2020-08-15 21:42:55 +0300

    rep

--- PRMbook.py
+++ PRMbook.py
@@ -1,26 +1,25 @@
 def prm_function():
-    print('Введите запрос, нажмите Enter. (выход - q)')
-    request_ssr = (input().upper())
+    request_ssr = (input('Введите запрос, нажмите Enter. (выход - q)\n').upper())
     list_counter = open('input.txt')
     counter = list_counter.readline()
-    list_ssr = open('input.txt', 'r', encoding='utf-8')
-    if request_ssr == 'Q' or request_ssr == 'Й':
-        return print('Выход!')
-    else:
-        counter_ssr = 0
-        for i in list_ssr:
-            i = i.strip().split()
-            if request_ssr == i[0]:
-                print(*i[1:])
-                counter_ssr = 0
-                prm_function()
-            elif counter_ssr == int(counter):
-                print('Вы ввели некорректное значение.\n'
-                      'Повторите пожалуйста.')
-                counter_ssr = 0
-                prm_function()
-            counter_ssr += 1
-
+    with open('input.txt', 'r', encoding='utf-8') as list_ssr:
+        if request_ssr == 'Q' or request_ssr == 'Й':
+            return print('Выход!')
+        else:
+            counter_ssr = 0
+            for i in list_ssr:
+                i = i.strip().split()
+                if request_ssr == i[0]:
+                    print(*i[1:])
+                    counter_ssr = 0
+                    prm_function()
+                elif counter_ssr == int(counter):
+                    print('Вы ввели некорректное значение.\n'
+                          'Повторите пожалуйста.')
+                    counter_ssr = 0
+                    prm_function()
+                counter_ssr += 1
+    list_counter.close()
 
 print('Добро пожаловать!\n'
       'Категории пассажиров (например введите WCHR или SVAN)\n'
